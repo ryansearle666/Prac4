8e395693124338f703d75fde71653aa2907712c2
ryan.searle <ryan.searle@my.jcu.edu.au>
2017-08-31 10:57:11 +1000

    Initial commit: Practical 5

---
+++ prac05.py
@@ -0,0 +1,37 @@
+"""
+CP1404/CP5632 Practical
+State names in a dictionary
+File needs reformatting
+"""
+# counts_of_words = {"a": 2, "collection": 1, "fun": 1, "is": 3, "it": 1, "nice": 1, "of": 2, "thing": 1, "this": 2, "words": 2}
+
+# # TODO: Reformat this file so the dictionary code follows PEP 8 convention
+# STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+#                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+# # print(STATE_NAMES)
+#
+# state = str.upper(input("Enter short state: "))
+# while state != "":
+#         if state in STATE_NAMES:
+#             print(state, "is", STATE_NAMES[state])
+#         else:
+#             print("Invalid short state")
+#         state = input("Enter short state: ")
+#
+#
+# for state in STATE_NAMES:
+#     print("{:<4} is {:>}".format(state, STATE_NAMES[state]))
+#
+#
+# Hexidecimal_colour_codes = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#ffefdb", "AntiqueWhite2": "#eedfcc", "aquamarine1": "#7fffd4"}
+#
+#
+# pick_colour = str(input("enter colour code: "))
+# while pick_colour != "magic":
+#     if pick_colour in Hexidecimal_colour_codes:
+#         print("{} gives {}".format(pick_colour, Hexidecimal_colour_codes[pick_colour]))
+#     else:
+#         print("invalid choice")
+#     pick_colour = str(input("enter colour code: "))
+#
+
